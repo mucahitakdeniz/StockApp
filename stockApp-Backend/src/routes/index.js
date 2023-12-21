81229188048adc8819d80bfeac801420fb05601c
mucahit <mucahitakdeniz28@gmail.com>
2023-12-21 13:55:36 +0300

    userRoute

--- stockApp-Backend/src/routes/index.js
+++ stockApp-Backend/src/routes/index.js
@@ -9,7 +9,7 @@ const router = require('express').Router()
 // // auth:
 // router.use('/auth', require('./auth'))
 // // user:
-// router.use('/users', require('./user'))
+router.use('/users', require('./user'))
 // // token:
 // router.use('/tokens', require('./token'))
 
--- stockApp-Backend/src/routes/user.js
+++ stockApp-Backend/src/routes/user.js
@@ -1,4 +1,15 @@
-"use strict"
+"use strict";
 
-const router = require('express').Router()
-/* ------------------------------------------------------- */
+const router = require("express").Router();
+
+const user = require("../controllers/user");
+
+router.route("/").get(user.list).post(user.create);
+router
+  .route("/:id")
+  .get(user.read)
+  .put(user.update)
+  .patch(user.create)
+  .delete(user.delete);
+
+  module.export=router
