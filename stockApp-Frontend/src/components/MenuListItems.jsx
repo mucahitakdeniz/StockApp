70efb1ccfd1ad695d10a26f5a5eafd50c255952f
mucahit <mucahitakdeniz28@gmail.com>
2024-01-01 17:06:40 +0300

    menuList

---
+++ stockApp-Frontend/src/components/MenuListItems.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import List from "@mui/material/List";
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import StoreIcon from '@mui/icons-material/Store';
+import SellIcon from '@mui/icons-material/Sell';
+import FactoryIcon from '@mui/icons-material/Factory';
+import { useNavigate } from "react-router-dom";
+
+
+const icons = [
+  {
+    icon: <SpaceDashboardIcon />,
+    title: "Dahboard",
+    url: "/stock",
+  },
+  {
+    icon: <LocalGroceryStoreIcon />,
+    title: "Purchase",
+    url: "/stock/purchase",
+  },
+  {
+    icon: <MonetizationOnIcon />,
+    title: "Sales",
+    url: "/stock/sales",
+  },
+  {
+    icon: <StoreIcon />,
+    title: "Firms",
+    url: "/stock/firms",
+  },
+  {
+    icon: <SellIcon />,
+    title: "Brands",
+    url: "/stock/brands",
+  },
+  {
+    icon: <FactoryIcon />,
+    title: "Products",
+    url: "/stock/products",
+  },
+];
+
+export const MenuListItems = () => {
+    const navigate =useNavigate()
+  return (
+    <div>
+      <List>
+        {icons.map((icon, index) => (
+          <ListItem key={index} disablePadding onClick={()=>navigate(icon.url)}>
+            <ListItemButton>
+              <ListItemIcon>
+                {icon.icon}
+              </ListItemIcon>
+              <ListItemText primary={icon.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
--- stockApp-Frontend/src/pages/Dashboard.jsx
+++ stockApp-Frontend/src/pages/Dashboard.jsx
@@ -1,31 +1,97 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { useSelector } from "react-redux";
 import useAuthCall from "../hooks/useAuthCall";
-import {useSelector } from "react-redux";
+import Button from "@mui/material/Button";
+import { MenuListItems } from "../components/MenuListItems";
 
-function Dashboard() {
+
+const drawerWidth = 220;
+
+export default function Dashboard() {
   const { logout } = useAuthCall();
   const currentUser = useSelector((state) => state.auth);
-
   return (
-    <Box sx={{ display: "flex" }}>
-      s
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar position="fixed">
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             STOCK APP
           </Typography>
-          {currentUser && <Button color="inherit" onClick={()=> logout()}>Logout</Button>}
+          {currentUser && (
+            <Button color="inherit" onClick={() => logout()}>
+              Logout
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+       <MenuListItems/>
+        <Divider />
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        <Toolbar />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+          sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Box>
     </Box>
   );
 }
 
-export default Dashboard;
