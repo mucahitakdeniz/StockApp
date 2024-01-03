bf72fe2c6632d83c7d8e486736dd5593010b144e
mucahit <mucahitakdeniz28@gmail.com>
2024-01-03 21:19:24 +0300

    cardsstyle

--- stockApp-Frontend/src/components/Cards.jsx
+++ stockApp-Frontend/src/components/Cards.jsx
@@ -1,108 +1,109 @@
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import useStockCall from "../hooks/useStockCall";
+import { btnStyle } from "../styles/globasStyles";
 
 const Cards = ({ data }) => {
-  const { deleteFunction } = useStockCall();
+  //const { deleteFunction } = useStockCall();
   return (
-    <Grid container spacing={2} sx={{ flexGrow: 1, marginTop: 3 }}>
-      {data.map((item, index) => (
-        <Grid item xs={12} sm={6} md={4} lg={4} key={index}>
+    <Grid container spacing={2} justifyContent={"center"}>
+      {data?.map((item, index) => (
+        <Grid item key={index}>
           <Card
             sx={{
               boxShadow: 17,
-              maxWidth: 350,
+              width: 350,
               height: 450,
               borderRadius: 4,
               margin: 2,
               padding: 1,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
             }}
           >
-            <CardContent>
-              <Typography
-                gutterBottom
-                variant="h5"
-                m={2}
-                color={"error"}
-                component="div"
-              >
-                {item?.name}
+            <Typography
+              gutterBottom
+              variant="h5"
+              m={2}
+              color={"error"}
+              component="div"
+            >
+              {item?.name}
+            </Typography>
+            {item?.address && (
+              <Typography variant="body2" color="text.secondary">
+                Adress : {item.address}
               </Typography>
-              {item?.address && (
-                <Typography variant="body2" color="text.secondary">
-                  Adress : {item.address}
-                </Typography>
-              )}
-              <br />
-              {item?.phone && (
-                <Typography variant="body2" color="text.secondary">
-                  Phone: {item.phone}
-                </Typography>
-              )}
-              <br />
-              {item?.price !== undefined && item.price !== 0 && (
-                <Typography variant="body2" color="text.secondary">
-                  Price: {item.price}
-                </Typography>
-              )}
-              {item?.price_total !== undefined && item?.price_total !== 0 && (
-                <Typography variant="body2" color="text.secondary">
-                  Total Price: {item.price_total}
-                </Typography>
-              )}
-              {item?.quantity !== undefined && item.quantity !== 0 && (
-                <Typography variant="body2" color="text.secondary">
-                  Quantity : {item.quantity}
-                </Typography>
-              )}
-              {item?.stock !== undefined && item.stock !== 0 && (
-                <Typography variant="body2" color="text.secondary">
-                  Stock: {item.stock}
-                </Typography>
-              )}
-              {item?.brand_id?.name && (
-                <Typography variant="body2" color="text.secondary">
-                  Brand: {item.brand_id.name}
-                </Typography>
-              )}
-              {item?.category_id?.name && (
-                <Typography variant="body2" color="text.secondary">
-                  Category: {item.category_id.name}
-                </Typography>
-              )}
-              {item?.product_id?.name && (
-                <Typography variant="body2" color="text.secondary">
-                  Product: {item.product_id.name}
-                </Typography>
-              )}
-            </CardContent>
+            )}
+            {item?.phone && (
+              <Typography variant="body2" color="text.secondary">
+                Phone: {item.phone}
+              </Typography>
+            )}
+            <br />
+            {item?.price !== undefined && (
+              <Typography variant="body2" color="text.secondary">
+                Price: {item.price}
+              </Typography>
+            )}
+            {item?.price_total !== undefined && (
+              <Typography variant="body2" color="text.secondary">
+                Total Price: {item.price_total}
+              </Typography>
+            )}
+            {item?.quantity !== undefined && (
+              <Typography variant="body2" color="text.secondary">
+                Quantity : {item.quantity}
+              </Typography>
+            )}
+            {item?.stock !== undefined && (
+              <Typography variant="body2" color="text.secondary">
+                Stock: {item.stock}
+              </Typography>
+            )}
+            {item?.brand_id?.name && (
+              <Typography variant="body2" color="text.secondary">
+                Brand: {item.brand_id.name}
+              </Typography>
+            )}
+            {item?.category_id?.name !== undefined && (
+              <Typography variant="body2" color="text.secondary">
+                Category: {item?.category_id?.name}
+              </Typography>
+            )}
+            {item?.product_id?.name !== undefined && (
+              <Typography variant="body2" color="text.secondary">
+                Product: {item.product_id.name}
+              </Typography>
+            )}
             {item?.image && (
               <CardMedia
                 component="img"
                 image={item.image}
                 title={item.name}
-                sx={{ maxWidth: 300, maxHeight: 200, padding: 5 }}
+                sx={{
+                  objectFit: "contain",
+                  maxHeight: 175,
+                  width: 200,
+                  margin:"auto"
+                }}
               />
             )}
-            <CardActions sx={{ display: "flex", justifyContent: "center" }}>
-              <Button size="small">
-                <ListItemIcon>
-                  <EditIcon />
-                </ListItemIcon>
-              </Button>
-              <Button size="small">
-                <ListItemIcon onClick={()=>deleteFunction('sales',item._id) }>
-                  <DeleteIcon  />
-                </ListItemIcon>
-              </Button>
+            <CardActions
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                m: 2,
+              }}
+            >
+              <EditIcon sx={btnStyle} />
+              <DeleteIcon sx={btnStyle} />
             </CardActions>
           </Card>
         </Grid>
