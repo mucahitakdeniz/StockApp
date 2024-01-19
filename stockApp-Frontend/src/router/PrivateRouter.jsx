29b342eb0ac1eab2888377d2643df84a44bd3815
mucahit <mucahitakdeniz28@gmail.com>
2024-01-19 15:18:51 +0300

    up

--- stockApp-Frontend/src/router/PrivateRouter.jsx
+++ stockApp-Frontend/src/router/PrivateRouter.jsx
@@ -1,10 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { toastWarnNotify } from "../helper/ToastNotify";
 
 const PrivateRouter = () => {
   const { currentUser } = useSelector((state) => state.auth);
-  toastWarnNotify("You must login");
   return currentUser ? <Outlet /> : <Navigate to="/" />;
 };
 
